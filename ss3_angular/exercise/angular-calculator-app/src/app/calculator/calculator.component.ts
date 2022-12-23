213e1825345a68c94eaf898479febce1a2e2ac1b
DanHuy77 <b77cwalk@gmail.com>
2022-12-23 13:37:39 +0700

    exercise

---
+++ ss3_angular/exercise/angular-calculator-app/src/app/calculator/calculator.component.ts
@@ -0,0 +1,62 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+  num1?: number;
+  num2?: number;
+  operator?: string;
+  operator2?: string;
+  result?: number;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line:typedef
+  changeNum1(num1: string) {
+    // tslint:disable-next-line:radix
+    this.num1 = parseInt(num1);
+  }
+
+  // tslint:disable-next-line:typedef
+  changeNum2(num2: string) {
+    // tslint:disable-next-line:radix
+    this.num2 = parseInt(num2);
+  }
+
+  // tslint:disable-next-line:typedef
+  changeOperator(operator: string) {
+    this.operator = operator;
+  }
+
+  // tslint:disable-next-line:typedef
+  calculate(operator2: string) {
+    this.operator2 = operator2;
+// tslint:disable-next-line:no-unused-expression
+    switch (this.operator) {
+      case '+':
+        // @ts-ignore
+        // tslint:disable-next-line:radix
+        this.result = this.num1 + this.num2;
+        break;
+      case '-':
+        // @ts-ignore
+        this.result = this.num1 - this.num2;
+        break;
+      case 'x':
+        // @ts-ignore
+        this.result = this.num1 * this.num2;
+        break;
+      case '/':
+        // @ts-ignore
+        this.result = this.num1 / this.num2;
+        break;
+    }
+  }
+}
---
+++ ss3_angular/exercise/angular-color-picker-app/src/app/color/color.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-color',
+  templateUrl: './color.component.html',
+  styleUrls: ['./color.component.css']
+})
+export class ColorComponent implements OnInit {
+  color?: string;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line:typedef
+  getColor(color: string) {
+    this.color = color;
+  }
+}
